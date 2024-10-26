c1c7baddfe3ca05b33cc7cb5f9d0acb3d3956663
shayan shirazian <shayan@colaskitchen.com>
2024-10-26 11:52:54 +0330

    1. Each task creates and can be added by "Save Task" btn
    2. when opening the app , add section is not shown and by clicking on "add btn" boxes appear and text can be inputed in the task list .

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,64 @@
 import React, {useState} from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
-
+import AddTask from './components/AddTask';
 
 const App = () => {
+    const [showAddTask, setShowAddTask] = useState<boolean>(false);
+
     const [tasks, setTasks] = useState([
         {
             id: 1,
-            text: 'Doctors Appoinment',
+            text: 'Doctors Appointment',
             day: 'Feb 5th at 2:30pm',
-            reminder: true
+            reminder: true,
         },
         {
             id: 2,
             text: 'Meeting at School',
             day: 'Feb 6th at 1:30pm',
-            reminder: true
+            reminder: true,
         },
         {
             id: 3,
             text: 'Food Shopping',
             day: 'Feb 7th at 2:30pm',
-            reminder: false
-        }
+            reminder: false,
+        },
     ]);
 
-    //Delete Task
+    // Add Task
+    const addTask = (task: any) => {
+        const id = Math.floor(Math.random() * 1000) + 1;
+        const newTask = {id, ...task};
+        setTasks([...tasks, newTask]);
+    };
+
+    // Delete Task
     const deleteTask = (id: number) => {
         setTasks(tasks.filter((task) => task.id !== id));
-    }
+    };
 
-    //toggle Reminder
+    // Toggle Reminder
     const toggleReminder = (id: number) => {
         setTasks(
             tasks.map((task) =>
-                task.id === id ? {
-                    ...task, reminder:
-                        !task.reminder
-                } : task))
-    }
+                task.id === id ? {...task, reminder: !task.reminder} : task
+            )
+        );
+    };
 
     return (
         <div className="container">
-
-            <Header/>
-            {tasks.length > 0 ?
-                < Tasks tasks={tasks}
-                        onDelete={deleteTask} onToggle={toggleReminder}
-                />
-                :
-                'No tasks found.'}
+            <Header onAdd={() => setShowAddTask(!showAddTask)}/>
+            {showAddTask && <AddTask onAdd={addTask}/>}
+            {tasks.length > 0 ? (
+                <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+            ) : (
+                'No tasks found.'
+            )}
         </div>
     );
-}
+};
 
 export default App;
