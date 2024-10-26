b7b97a64677991c50a976fba47b69955e9a9ec1f
shayan shirazian <shayan@colaskitchen.com>
2024-10-26 10:53:00 +0330

    Delete Function added(not Delete btn functionallity)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 
 const App = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Doctors Appoinment',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true
+        },
+        {
+            id: 2,
+            text: 'Meeting at School',
+            day: 'Feb 6th at 1:30pm',
+            reminder: true
+        },
+        {
+            id: 3,
+            text: 'Food Shopping',
+            day: 'Feb 7th at 2:30pm',
+            reminder: false
+        }
+    ]);
+
+    //Delete Task
+    const deleteTask = (id: number) => {
+        console.log('deleteTask', id);
+    }
+
     return (
         <div className="container">
 
             <Header/>
-            <Tasks/>
+            <Tasks tasks={tasks}
+                   onDelete={deleteTask}/>
         </div>
     );
 }
---
+++ src/components/Task.js
@@ -0,0 +1,25 @@
+import {FaTimes} from "react-icons/fa";
+
+const Task = ({task, onDelete}) => {
+    return (
+        <div className="task">
+            <h3>
+                {task.text}
+                {/*adds x icon for deleting*/}
+                <FaTimes
+                    style={{
+                        color: 'red',
+                        cursor: 'pointer',
+                    }}
+                    onClick={() =>
+                        onDelete(task.id)
+                    }
+                />
+            </h3>
+            <p>{task.day}</p>
+        </div>
+    )
+}
+
+
+export default Task;
