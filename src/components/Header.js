b124a19527d7f0958a62189de886a874e7340279
shayan shirazian <shayan@colaskitchen.com>
2024-10-26 09:45:30 +0330

    styles added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-const Header = (props) => {
-  return (
-    <header>
-      <h1>{props.title}</h1>
-    </header>
-  );
+const Header = ({title}) => {
+    return (
+        <header>
+            <h1 style={headingStyles}>{title}</h1>
+        </header>
+    );
 };
 Header.defaultProps = {
     title: 'Task Tracker',
 }
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+//CSS in JS
+// const headingStyles = {
+//     color: 'red',
+//     backgroundColor: 'black',
+// }
 
 export default Header;
