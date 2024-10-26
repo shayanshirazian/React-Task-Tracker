a3ab6d5a5ee95e1073133b34d3de9165cd480878
shayan shirazian <shayan@colaskitchen.com>
2024-10-26 09:50:46 +0330

    Button added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
-import React from 'react';
 import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({title}) => {
     return (
-        <header>
-            <h1 style={headingStyles}>{title}</h1>
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Hello'/>
         </header>
     );
 };
