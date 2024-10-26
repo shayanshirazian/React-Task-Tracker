e204656e6ce0b12a4585c5ae146260a19daee875
shayan shirazian <shayan@colaskitchen.com>
2024-10-26 09:28:45 +0330

    first Setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Header from './components/Header';
 
 
-function App() {
-  return (
-    <div className="container">
-        <h1>Hello from React</h1>
-     <Header/>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="container">
+            <h1>Hello from React</h1>
+            <Header/>
+        </div>
+    );
 }
 
 export default App;
