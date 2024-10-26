2f9021bd4166d061c0f032799744e9af9f4d987f
shayan shirazian <shayan@colaskitchen.com>
2024-10-26 10:12:56 +0330

    Add Click Btn Funcation

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,27 @@
-const Button = () => {
+import PropTypes from "prop-types";
+
+const Button = ({color, text, onClick}) => {
+
     return (
-        <button className='btn'>Add</button>
+
+        <button
+            onClick={onClick}
+            style={{backgroundColor: color}}
+            className='btn'>
+            {text}
+        </button>
 
     );
 };
+
+Button.defaultProps = {
+    color: 'steelblue',
+    text: 'Add'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+}
 
 export default Button;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,18 @@ import PropTypes from "prop-types";
 import Button from "./Button";
 
 const Header = ({title}) => {
+    const onClick = () => {
+        console.log('click')
+    }
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Hello'/>
+            <Button color='green' text='Add' onClick={onClick}/> {/* Corrected here */}
+
         </header>
     );
 };
+
 Header.defaultProps = {
     title: 'Task Tracker',
 }
@@ -16,11 +21,5 @@ Header.defaultProps = {
 Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
-
-//CSS in JS
-// const headingStyles = {
-//     color: 'red',
-//     backgroundColor: 'black',
-// }
 
 export default Header;
